00e28bdc549dcb20d3086f39ede0c92d966ec3a8
Garrett Griffin-Morales <grgisme@gmail.com>
2024-06-15 14:19:27 -0400

    chore: Fixing package.json

--- src/models/turn.js
+++ src/models/turn.js
@@ -1,26 +1,12 @@
-const { Expose, plainToInstance } = require('class-transformer');
+const { Expose, plainToClass } = require('class-transformer');
 
-/**
- * @typedef {Object} Turn
- * @property {number} turn
- * @property {string} [month]
- * @property {number} [year]
- */
 class Turn {
     @Expose() turn;
     @Expose() month;
     @Expose() year;
 
-    /**
-     * @param {Object} data
-     * @returns {Turn}
-     */
     static modelValidate(data) {
-        try {
-            return /** @type {Turn} */ plainToInstance(Turn, data);
-        } catch (errors) {
-            throw new Error('Validation failed: ' + errors);
-        }
+        return plainToClass(Turn, data);
     }
 }
 
