8b949abf7cb71a55f762046586606a72d0c72bd2
Garrett Griffin-Morales <grgisme@gmail.com>
2024-06-15 14:59:56 -0400

    Polishing turn objects and turn test.

--- src/models/turn.js
+++ src/models/turn.js
@@ -1,12 +1,23 @@
 const { Expose, plainToClass } = require('class-transformer');
+const { IsInt, IsString, IsOptional } = require('class-validator');
 
+/**
+ * @typedef {Object} Turn
+ * @property {number} turn
+ * @property {string} [month]
+ * @property {number} [year]
+ */
 class Turn {
-    @Expose() turn;
-    @Expose() month;
-    @Expose() year;
+    @Expose() @IsInt turn;
+    @Expose() @IsString @IsOptional month;
+    @Expose() @IsInt @IsOptional year;
 
+    /**
+     * @param {Object} data
+     * @returns {Turn}
+     */
     static modelValidate(data) {
-        return plainToClass(Turn, data);
+        return /** @type { Turn } */ plainToClass(Turn, data);
     }
 }
 
